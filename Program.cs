0943f7af252ebdb7c0c490703fe9ed2ac3439f79
shoparla09 <2242356@talnet.nl>
2026-01-04 16:28:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
